008aab9ab74bd37f5d2eafa7b3371ceffc6fce7e
Charles Liu <charlesliu688@gmail.com>
2023-07-12 17:37:52 -0700

    basically functional wheel of fortune with proper animation and graphics implemented

--- SupriseMeal/suprisemeal/src/App.js
+++ SupriseMeal/suprisemeal/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import Wheel from './FortuneWheel.js'
 import ReactWheel from './ReactWheel';
+import WheelComponent from "./ReactWheel";
+/**
 
 function App() {
     /** 
@@ -22,9 +23,54 @@ function App() {
         </a>
       </header>
     </div>
-  );*/
-    return <Wheel/>;
+  );
+    return <ReactWheel/>;
 }
 
-
 export default App;
+*/
+
+
+export default function App() {
+    const segments = [
+        "Better luck ",
+        "10% off",
+        "5% off",
+        "Better luck ",
+        "20% off",
+        "15% off"
+    ];
+    const segColors = [
+        "black",
+        "#60BA97",
+        "black",
+        "#60BA97",
+        "black",
+        "#60BA97"
+    ];
+    const onFinished = (winner) => {
+        console.log(winner);
+    };
+    return (
+        <div className="App">
+            <h1>Spinner wheel Demo for TDC</h1>
+            <div>
+                <WheelComponent
+                    segments={segments}
+                    segColors={segColors}
+                    winningSegment="MM"
+                    onFinished={(winner) => onFinished(winner)}
+                    primaryColor="black"
+                    contrastColor="white"
+                    buttonText="Start"
+                    isOnlyOnce={false}
+                    size={190}
+                    upDuration={500}
+                    downDuration={600}
+                    fontFamily="Helvetica"
+                />
+            </div>
+            <h2> Spin the wheel and win exiting offers</h2>
+        </div>
+    );
+}
